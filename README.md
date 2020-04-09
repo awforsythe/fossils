1ff23d3ac1c0ef9368a81c7783d939de9a2db8bc
Alex Forsythe <awforsythe@gmail.com>
2020-04-08 19:37:16 -0500

    Add basic application layer (app), to serve the fossil tracking API

---
+++ README.md
@@ -0,0 +1,12 @@
+# fossils
+
+A handy tool for tracking fossil collection among friends in Animal Crossing: New Horizons.
+
+## app
+
+To run the app locally:
+
+* `cd app`
+* `npm install`
+* `npm run start:dev`
+* http://localhost:8080
---
+++ app/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const expressWs = require('express-ws')(app, server);
+
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api', (req, res, next) => res.send({ message: 'Hello world' }));
+
+app.use((err, req, res, next) => {
+  if (!err.message || !err.code || err.code >= 500) {
+    next(err);
+  } else {
+    res.status(err.code).send({ error: err.message });
+  }
+});
+
+module.exports = { app, server };
