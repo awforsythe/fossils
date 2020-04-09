ad20e79779df5921c812103e36414a6a5922f817
Alex Forsythe <awforsythe@gmail.com>
2020-04-09 10:17:49 -0500

    Add basic query interpolation
    We can write SQL literals with named-variable placeholders, e.g.:
      SELECT * FROM foo WHERE id = $id AND value = $whatever;
    
    ...and then when adding our runQuery middleware to a route, we can
    specify that query string along with a params object, or a function
    that resolves the params given the request, e.g.:
      (req) => ({ id: parseInt(req.params.id), whatever: 'bar' })
    
    When the runQuery thunk is called, it'll use interpolateQuery to
    generate the two arguments, properly sanitized, to pass to postgres:
      ('SELECT * FROM foo WHERE id = $1 AND value = $2', [6, 'bar'])

--- app/db.js
+++ app/db.js
@@ -2,12 +2,49 @@ const { Pool } = require('pg');
 
 const pool = new Pool();
 
+function interpolateQuery(queryStr, paramsObj) {
+  // Copy the string so we can modify it in-place (this kind of string manipulation isn't super optimized, but eh)
+  let query = (' ' + queryStr).slice(1);
+  
+  // Short-circuit optimization: if the query has no no named variables, skip interpolation
+  const regexp = /\$([a-zA-Z_]\w+)/g;
+  if (!regexp.exec(queryStr)) {
+    return [queryStr, []];
+  }
+
+  // Run a loop to replace named parameters with $1, $2, $3, etc., and build an accompanying array of values
+  let params = [];
+  while (true) {
+    // Keep matching named variables ($param) until we've replaced them all with numeric sentinels ($1)
+    regexp.lastIndex = 0;
+    const match = regexp.exec(query);
+    if (!match) {
+      break;
+    }
+
+    // Add the corresponding value (or null) to the params array, and replace all occurrences in the query string
+    const value = paramsObj[match[1]];
+    params.push(value !== undefined ? value : null);
+    query = query.replace(new RegExp('\\$' + match[1], 'g'), '$' + params.length);
+  }
+
+  return [query, params];
+}
+
 module.exports = {
-  runQuery: (query, params) => (
-    async (req, res, next) => {  
+  runQuery: (query, paramsFunc) => (
+    async (req, res, next) => {
+      let params;
+      try {
+        params = paramsFunc ? paramsFunc(req) : null;
+      } catch (err) {
+        console.log(err);
+        return res.status(400).send({ error: err });
+      }
+  
       let r;
       try {
-        r = await pool.query(query, params);
+        r = await pool.query(...interpolateQuery(query, params));
       } catch (err) {
         console.log(err);
         return res.status(500).send({ error: err.stack });
