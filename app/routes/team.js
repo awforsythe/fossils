7fba51e46c4422474a6a2a00a2efa856c19c8e68
Alex Forsythe <awforsythe@gmail.com>
2020-04-10 13:17:53 -0500

    Add API routes for update have list; include pieces arrays in GET /team

--- app/routes/team.js
+++ app/routes/team.js
@@ -89,16 +89,62 @@ router.post('/:code/player/:id', [
   }
 );
 
+router.post('/:code/player/:id/have/:pieceId', [
+    param('code').isString(),
+    param('id').isInt().toInt(),
+    param('pieceId').isInt().toInt(),
+  ],
+  validateParams,
+  runQuery(`
+    WITH p AS (
+      WITH t AS (
+        SELECT id FROM fossil.team WHERE code = $code
+      )
+      SELECT id FROM fossil.player WHERE team_id = (SELECT id FROM t) AND id = $id
+    )
+    INSERT INTO fossil.have (player_id, piece_id) VALUES ((SELECT id FROM p), $pieceId)
+    ON CONFLICT DO NOTHING;`,
+    req => req.params
+  ),
+  (req, res, next) => {
+    res.status(204).send();
+  }
+);
+
+router.post('/:code/player/:id/lose/:pieceId', [
+    param('code').isString(),
+    param('id').isInt().toInt(),
+    param('pieceId').isInt().toInt(),
+  ],
+  validateParams,
+  runQuery(`
+    WITH p AS (
+      WITH t AS (
+        SELECT id FROM fossil.team WHERE code = $code
+      )
+      SELECT id FROM fossil.player WHERE team_id = (SELECT id FROM t) AND id = $id
+    )
+    DELETE FROM fossil.have WHERE player_id = (SELECT id FROM p) AND piece_id = $pieceId;`,
+    req => req.params
+  ),
+  (req, res, next) => {
+    res.status(204).send();
+  }
+);
+
 router.get('/:code',
   runQuery(`
-    WITH t AS (SELECT * FROM fossil.TEAM WHERE code = $code)
+    WITH t AS (SELECT * FROM fossil.team WHERE code = $code)
     SELECT
       (SELECT id FROM t) AS team_id,
       (SELECT name FROM t) as team_name,
       (SELECT code FROM t) as team_code,
-      id AS player_id,
-      name AS player_name
-    FROM fossil.player WHERE team_id = (SELECT id FROM t) ORDER BY player.id;`,
+      player.id AS player_id,
+      player.name AS player_name,
+      piece_id
+    FROM fossil.player LEFT JOIN fossil.have ON have.player_id = player.id
+      WHERE team_id = (SELECT id FROM t)
+    ORDER BY player.id;`,
     req => req.params,
   ),
   (req, res, next) => {
@@ -107,8 +153,20 @@ router.get('/:code',
       return res.status(404).send({ message: `No team found with code '${req.params.code}'` });
     }
     let players = [];
+    let player = { id: null, name: null, pieces: [] };
     for (const row of rows) {
-      players.push({ id: row.player_id, name: row.player_name });
+      if (row.player_id !== player.id) {
+        if (player.id) {
+          players.push(player);
+        }
+        player = { id: row.player_id, name: row.player_name, pieces: [] };
+      }
+      if (row.piece_id) {
+        player.pieces.push(row.piece_id);
+      }
+    }
+    if (player.id) {
+      players.push(player);
     }
     res.send({ id: rows[0].team_id, name: rows[0].team_name, code: rows[0].team_code, players });
   }
