1ba8e046e0b5061713d64bd09e256ebf75da308a
Alex Forsythe <awforsythe@gmail.com>
2020-04-11 14:38:36 -0500

    Simplify SQL queries used in team API
    The database operations in our API routes naturally involve some
    indirection based on the fact that users supply a team code for basic
    authentication:
    
    - For all team-related routes, we need to look up the relevant team
      from its string code, and abort immediately if there's no such team
    
    - When updating player state (including piece_ids in the fossil.have
      association table), we need to validate that request contains the
      right team code for the given player ID
    
    - When querying team data, we need to do multiple lookups:
      - Get team row (including team id) from team code
      - With that team id: Get player rows (including player id)
      - With those player ids: Get piece ids for each player
    
    Previously, we attempted to cram all this validation and these multiple
    lookups into a single statement for each route. This turned out to be a
    misguided way of implicitly building atomicity into our routes. If we
    need our API operations to be atomic, we can just use actual SQL
    transactions.
    
    This change breaks several of our overly-complex SQL queries into
    multiple steps. Rather than wrapping a kitchen-sink query into a single
    middleware function within the route, we instead use the new
    validateTeamCode or validatePlayerId functions to handle prerequisite
    validation, and return a 404 if unable to proceed.
    
    Before-and-after profiling results for GET /api/team/:code are here:
    https://pastebin.com/raw/gCR3wa8G
    
    In short, a GET request for a single team's data used to look like this:
    
        WITH t AS (SELECT * FROM fossil.team WHERE code = '833HZFubQ')
        SELECT
          (SELECT id FROM t) AS team_id,
          (SELECT name FROM t) AS team_name,
          (SELECT code FROM t) AS team_code,
          player.id AS player_id,
          player.name AS player_name,
          piece_id
        FROM fossil.player LEFT JOIN fossil.have
          ON have.player_id = player.id
        WHERE team_id = (SELECT id FROM t) ORDER BY player.id;
    
    Whereas we now run three separate queries:
    
        SELECT * FROM fossil.team WHERE code = '833HZFubQ';
        SELECT id, name FROM fossil.player WHERE team_id = 3;
        SELECT player_id, json_agg(piece_id) AS pieces FROM fossil.have
          WHERE player_id IN (4, 5, 3, 6, 7, 8, 9, 10, 11, 12)
          GROUP BY player_id;
    
    The result is slightly faster to execute in PostgreSQL, but the more
    significant factor is that it returns far less duplicated data, and the
    results are formatted much more closely to how the API represents them,
    so there's a lot less mess in the backend app.

--- app/routes/team.js
+++ app/routes/team.js
@@ -2,8 +2,29 @@ const express = require('express');
 const router = express.Router();
 const shortid = require('shortid');
 
-const { runQuery } = require('../db');
+const { db, runQuery } = require('../db');
 const { param, body, validateParams } = require('../validation');
+
+async function validateTeamCode(req, res, next) {
+  const r = await db.query('SELECT id FROM fossil.team WHERE code = $1;', [req.params.code]);
+  if (r.rowCount !== 1) {
+    return res.status(404).send({ message: `No team found with code '${req.params.code}'` });
+  }
+  req.teamId = r.rows[0].id;
+  next();
+}
+
+async function validatePlayerId(req, res, next) {
+  const r = await db.query(`
+    WITH t AS (SELECT id FROM fossil.team WHERE code = $1)
+    SELECT id FROM fossil.player WHERE team_id = (SELECT id FROM t)
+    AND id = $2;
+  `, [req.params.code, req.params.id]);
+  if (r.rowCount !== 1) {
+    return res.status(404).send({ message: `No player with ID ${req.params.id} belongs to a team with code '${req.params.code}'`});
+  }
+  next();
+}
 
 router.post('/', [
     body('teamName').isString().isLength({ min: 1, max: 128 }),
@@ -20,7 +41,7 @@ router.post('/', [
       (SELECT code FROM t) as team_code,
       id AS player_id,
       name AS player_name;`,
-    (req) => ({ code: shortid.generate(), ...req.body })
+    req => ({ code: shortid.generate(), ...req.body })
   ),
   (req, res, next) => {
     const row = req.queryResult.rows[0];
@@ -39,7 +60,7 @@ router.post('/:code', [
     UPDATE fossil.team SET name = $name
       WHERE code = $code
     RETURNING *;`,
-    (req) => ({ code: req.params.code, name: req.body.name })
+    req => ({ code: req.params.code, name: req.body.name })
   ),
   (req, res, next) => {
     if (req.queryResult.rowCount <= 0) {
@@ -50,16 +71,16 @@ router.post('/:code', [
 );
 
 router.post('/:code/player', [
-    param('code').isString().isLength({ min: 1, max: 128 }),
+    param('code').isString(),
     body('name').isString().isLength({ min: 1, max: 128 }),
   ],
   validateParams,
+  validateTeamCode,
   runQuery(`
-    WITH t AS (SELECT id FROM fossil.team WHERE code = $code)
     INSERT INTO fossil.player (team_id, name) VALUES 
-      ((SELECT id FROM t), $name)
+      ($teamId, $name)
     RETURNING *;`,
-    (req) => ({ code: req.params.code, name: req.body.name })
+    req => ({ teamId: req.teamId, name: req.body.name })
   ),
   (req, res, next) => {
     const player = req.queryResult.rows[0];
@@ -68,16 +89,15 @@ router.post('/:code/player', [
 );
 
 router.post('/:code/player/:id', [
-    param('code').isString().isLength({ min: 1, max: 128 }),
+    param('code').isString(),
     param('id').isInt().toInt(),
     body('name').isString().isLength({ min: 1, max: 128 }),
   ],
   validateParams,
-  runQuery(`
-    WITH t AS (SELECT id FROM fossil.team WHERE code = $code)
-    UPDATE fossil.player SET name = $name
-    WHERE team_id = (SELECT id FROM t) AND id = $id RETURNING *;`,
-    (req) => ({ code: req.params.code, id: req.params.id, name: req.body.name })
+  validatePlayerId,
+  runQuery(
+    `UPDATE fossil.player SET name = $name WHERE id = $id RETURNING *;`,
+    req => ({ id: req.params.id, name: req.body.name })
   ),
   (req, res, next) => {
     if (req.queryResult.rowCount <= 0) {
@@ -95,14 +115,10 @@ router.post('/:code/player/:id/have/:pieceId', [
     param('pieceId').isInt().toInt(),
   ],
   validateParams,
+  validatePlayerId,
   runQuery(`
-    WITH p AS (
-      WITH t AS (
-        SELECT id FROM fossil.team WHERE code = $code
-      )
-      SELECT id FROM fossil.player WHERE team_id = (SELECT id FROM t) AND id = $id
-    )
-    INSERT INTO fossil.have (player_id, piece_id) VALUES ((SELECT id FROM p), $pieceId)
+    INSERT INTO fossil.have (player_id, piece_id)
+      VALUES ($id, $pieceId)
     ON CONFLICT DO NOTHING;`,
     req => req.params
   ),
@@ -117,14 +133,9 @@ router.post('/:code/player/:id/lose/:pieceId', [
     param('pieceId').isInt().toInt(),
   ],
   validateParams,
+  validatePlayerId,
   runQuery(`
-    WITH p AS (
-      WITH t AS (
-        SELECT id FROM fossil.team WHERE code = $code
-      )
-      SELECT id FROM fossil.player WHERE team_id = (SELECT id FROM t) AND id = $id
-    )
-    DELETE FROM fossil.have WHERE player_id = (SELECT id FROM p) AND piece_id = $pieceId;`,
+    DELETE FROM fossil.have WHERE player_id = $id AND piece_id = $pieceId;`,
     req => req.params
   ),
   (req, res, next) => {
@@ -132,43 +143,35 @@ router.post('/:code/player/:id/lose/:pieceId', [
   }
 );
 
-router.get('/:code',
-  runQuery(`
-    WITH t AS (SELECT * FROM fossil.team WHERE code = $code)
-    SELECT
-      (SELECT id FROM t) AS team_id,
-      (SELECT name FROM t) as team_name,
-      (SELECT code FROM t) as team_code,
-      player.id AS player_id,
-      player.name AS player_name,
-      piece_id
-    FROM fossil.player LEFT JOIN fossil.have ON have.player_id = player.id
-      WHERE team_id = (SELECT id FROM t)
-    ORDER BY player.id;`,
-    req => req.params,
-  ),
-  (req, res, next) => {
-    const { rowCount, rows } = req.queryResult;
-    if (rowCount <= 0) {
+router.get('/:code', [
+    param('code').isString(),
+  ],
+  validateParams,
+  async (req, res, next) => {
+    const teamResult = await db.query('SELECT * FROM fossil.team WHERE code = $1;', [req.params.code]);
+    if (teamResult.rowCount !== 1) {
       return res.status(404).send({ message: `No team found with code '${req.params.code}'` });
     }
+    const playersResult = await db.query('SELECT id, name FROM fossil.player WHERE team_id = $1;', [teamResult.rows[0].id]);
+    if (playersResult.rowCount === 0) {
+      return res.send({ ...teamResult.rows[0], players: [] });
+    }
+    const playerIds = playersResult.rows.map(x => x.id);
+    const haveResult = await db.query(`
+      SELECT player_id, json_agg(piece_id) AS pieces FROM fossil.have
+        WHERE player_id IN (${playerIds.map((_, i) => `$${i + 1}`).join(', ')})
+      GROUP BY player_id;
+    `, playerIds);
+
     let players = [];
-    let player = { id: null, name: null, pieces: [] };
-    for (const row of rows) {
-      if (row.player_id !== player.id) {
-        if (player.id) {
-          players.push(player);
-        }
-        player = { id: row.player_id, name: row.player_name, pieces: [] };
-      }
-      if (row.piece_id) {
-        player.pieces.push(row.piece_id);
-      }
+    for (const playerRow of playersResult.rows) {
+      const haveRow = haveResult.rows.find(x => x.player_id == playerRow.id);
+      const pieces = haveRow ? haveRow.pieces : [];
+      players.push({ ...playerRow, pieces });
     }
-    if (player.id) {
-      players.push(player);
-    }
-    res.send({ id: rows[0].team_id, name: rows[0].team_name, code: rows[0].team_code, players });
+
+    console.log(haveResult.rows)
+    res.send({ ...teamResult.rows[0], players });
   }
 );
 
